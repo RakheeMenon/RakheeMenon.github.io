c6f21bb6a4d5e7f20196ba3857892706279ad9ac
RakheeMenon <RakheeMenon@users.noreply.github.com>
2017-05-04 15:07:07 +0530

    Add files via upload

---
+++ solution/js/byscript.js
@@ -0,0 +1,11 @@
+
+(function(window){
+	var byeSpeaker={};
+var speakWord="Good Bye";
+
+ byeSpeaker.speak= function (name){
+	console.log(speakWord + " " +byeSpeaker.name);
+}
+window.byeSpeaker=byeSpeaker;
+
+})(window);
